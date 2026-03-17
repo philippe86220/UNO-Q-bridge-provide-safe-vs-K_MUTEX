02281cf0ba8fead0730361be2d85c29f6032f74b
philippe86220 <142721115+philippe86220@users.noreply.github.com>
2026-03-17 15:28:21 +0100

    Implement mutexes for counter updates and monitoring

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -1,1 +1,54 @@
+#include <Arduino.h>
+#include <Arduino_RouterBridge.h>
+#include <zephyr/kernel.h>
 
+volatile int counter = 0;
+volatile int doubleValue = 0;
+
+K_MUTEX_DEFINE(data_mutex);
+K_MUTEX_DEFINE(monitor_mutex);
+
+void updateCounter(int x) {
+  k_mutex_lock(&data_mutex, K_FOREVER);
+
+  counter = x;
+  delay(5);
+  doubleValue = x * 2;
+
+  k_mutex_unlock(&data_mutex);
+
+  k_mutex_lock(&monitor_mutex, K_FOREVER);
+  Monitor.print("RPC update: counter=");
+  Monitor.print(counter);
+  Monitor.print(" double=");
+  Monitor.println(doubleValue);
+  k_mutex_unlock(&monitor_mutex);
+}
+
+void setup() {
+  Bridge.begin();
+  Monitor.begin();
+
+  Bridge.provide("update", updateCounter);
+}
+
+void loop() {
+  int c;
+  int d;
+
+  k_mutex_lock(&data_mutex, K_FOREVER);
+  c = counter;
+  d = doubleValue;
+  k_mutex_unlock(&data_mutex);
+
+  if (d != c * 2) {
+    k_mutex_lock(&monitor_mutex, K_FOREVER);
+    Monitor.print("ERROR inconsistent state: counter=");
+    Monitor.print(c);
+    Monitor.print(" double=");
+    Monitor.println(d);
+    k_mutex_unlock(&monitor_mutex);
+  }
+
+  delay(100);
+}
